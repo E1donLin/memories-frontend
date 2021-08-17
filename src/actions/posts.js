220521cafc4ebdc95f1d34d9472d152a39a2c051
YuJun <yl4039@rit.edu>
2021-08-17 17:15:29 -0400

    updated public folder

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -55,7 +55,7 @@ export const createPost = (post, history) => async (dispatch) => {
     dispatch({ type: START_LOADING })
     const { data } = await api.createPost(post)
 
-    history.push(`/posts/${data._id}`)
+    // history.push(`/posts/${data._id}`)
 
     dispatch({ type: CREATE, payload: data })
     dispatch({ type: END_LOADING })
